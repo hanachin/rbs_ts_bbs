7cf079b1697c252a826bdc220ed6e660850c0c37
Seiei Miyagi <hanachin@gmail.com>
2020-06-15 01:14:05 +0900

    Add not null constraint to boards.title

--- db/migrate/20200614160451_create_boards.rb
+++ db/migrate/20200614160451_create_boards.rb
@@ -1,7 +1,7 @@
 class CreateBoards < ActiveRecord::Migration[6.1]
   def change
     create_table :boards do |t|
-      t.string :title
+      t.string :title, null: false
 
       t.timestamps
     end
